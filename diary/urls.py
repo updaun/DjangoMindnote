d532f5ff6436fb2c594b9220e30443ee3d147ed7
updaun <updauney@daum.net>
2021-09-26 23:26:30 +0900

    urls setting

---
+++ diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.index),
+    # path('info/', views.info),
+    # path('write/', views.page_create),
+    # path('page/<int:page_id>/', views.page_detail),
+    # path('page/<int:page_id>/edit', views.page_update),
+    # path('page/<int:page_id>/delete', views.page_delete),
+]
