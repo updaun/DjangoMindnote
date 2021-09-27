1a8c1caed7ea26e281eee6fc969a877bf6d63164
updaun <updauney@daum.net>
2021-09-27 15:35:18 +0900

    add Page models

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateField(verbose_name="Page Created", auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
