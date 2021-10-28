20957ace47592a7e438949a17586d18659c88b34
updaun <updauney@daum.net>
2021-10-28 19:08:04 +0900

    page detail

--- diary/urls.py
+++ diary/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path('info/', views.info),
     path('diary/', views.page_list),
     # path('write/', views.page_create),
-    # path('page/<int:page_id>/', views.page_detail),
+    path('diary/<int:page_id>/', views.page_detail, name="page-detail"),
     # path('page/<int:page_id>/edit', views.page_update),
     # path('page/<int:page_id>/delete', views.page_delete),
 ]
