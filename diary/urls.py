a114a318bab796834520ed0d77789c3cb583d08a
updaun <updauney@daum.net>
2021-10-31 19:12:24 +0900

    url update

--- diary/urls.py
+++ diary/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index),
-    # path('info/', views.info),
-    path('diary/', views.page_list),
-    # path('write/', views.page_create),
-    path('diary/<int:page_id>/', views.page_detail, name="page-detail"),
-    # path('page/<int:page_id>/edit', views.page_update),
-    # path('page/<int:page_id>/delete', views.page_delete),
+    path('diary/', views.page_list, name='page-list'),
+    # path('diary/info/', views.info, name='info'),
+    # path('diary/write/', views.page_create, name='page-create'),
+    path('diary/page/<int:page_id>/', views.page_detail, name='page-detail'),
+    # path('diary/page/<int:page_id>/edit/', views.page_update, name='page-update'),
+    # path('diary/page/<int:page_id>/delete/', views.page_delete, name='page-delete'),
 ]
